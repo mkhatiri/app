9d2dd0eb957ca2251a7f2700e2c9c82a11e0d81b
Med <sesa676570@se.com>
2025-01-14 10:25:36 +0100

    add pylint step

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ def hello_world():
     return '<center><h1>Hello, m2i!  (2025)</h1></center>'
 
 if __name__ == "__main__":
-    """
-    Main entry point of the application.
-    """
+    #    Main entry point of the application.
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True,host='0.0.0.0',port=port)
