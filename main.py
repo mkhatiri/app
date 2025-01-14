1c50da158bed71f5226470594ff3250a98912bbd
Med <sesa676570@se.com>
2025-01-14 10:19:51 +0100

    add pylint step

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    """
+    A simple route that returns a greeting.
+    """
     return '<center><h1>Hello, m2i!  (2025)</h1></center>'
 
 if __name__ == "__main__":
+    """
+    Main entry point of the application.
+    """
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True,host='0.0.0.0',port=port)
